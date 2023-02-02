b66f91bd1945a072ea8d200ba934e7756d289bbf
Punya2208 <punyachandrashekar436@gmail.com>
2023-02-02 04:29:20 -0800

    pushhhhhhhh

---
+++ program1.java
@@ -0,0 +1,27 @@
+package abc;
+import java.util.ArrayList;
+public class qwe {
+	public static void India()
+	{
+		ArrayList<String> l1= new ArrayList<String>();
+				l1.add("Karnataka");
+		        l1.add("goa");
+		    	l1.add("Kerala");
+		    	l1.add("Delhi");
+		    	l1.add("Tamilnadu");
+		    	System.out.println("li");		
+	}
+	public static void Karnataka()
+	{
+		ArrayList<String> l2= new ArrayList<String>();
+		l2.add("Banglore");
+		l2.add("Banglore");
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
